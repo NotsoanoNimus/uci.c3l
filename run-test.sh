2ee315ffecf098c8b8b8d9f7046ad605de26a5a7
Zack Puhl <github@xmit.xyz>
2026-02-25 12:38:21 +0000

    fix positions from new games, and allow moves to be played from the current position

--- run-test.sh
+++ run-test.sh
@@ -3,30 +3,34 @@
 # Tests run with Stockfish by default. The submodule should always remain pinned to a stable release.
 #
 #
-TEST_OPTS="-O${1:-0} --libdir .. --lib uci"
+TEST_OPTS="-O${2:-0} --libdir .. --lib uci"
 
 echo -e "\n===== Running unit tests..."
 c3c compile-test ${TEST_OPTS} ./test/unit/ || { echo "Unit tests failed. Aborting." && exit 1; }
 
-STOCKFISH="${STOCKFISH:-./test/stockfish/src/stockfish}"
-echo -e "\n\n===== Attempting to test with Stockfish..."
-if [[ ! -x ${STOCKFISH} ]]; then
-	echo "ERROR: Please install/update and compile Stockfish in order to run tests."
-	echo
-	exit 1
+if [[ $# -eq 0 || "${1,,}" == "stockfish" ]]; then
+	STOCKFISH="${STOCKFISH:-./test/stockfish/src/stockfish}"
+	echo -e "\n\n===== Attempting to test with Stockfish..."
+	if [[ ! -x ${STOCKFISH} ]]; then
+		echo "ERROR: Please install/update and compile Stockfish in order to run tests."
+		echo
+		exit 1
+	fi
+
+	c3c compile-run ${TEST_OPTS} ./test/stockfish.c3 -- "${STOCKFISH}" \
+		|| { echo "Stockfish test failed. Aborting." && exit 1; }
 fi
 
-c3c compile-run ${TEST_OPTS} ./test/stockfish.c3 -- "${STOCKFISH}" \
-	|| { echo "Stockfish test failed. Aborting." && exit 1; }
 
+if [[ $# -eq 0 || "${1,,}" == "leela" ]]; then
+	LC0="${LC0:-./test/lc0/build/release/lc0}"
+	echo -e "\n\n===== Attempting to test with Leela (lc0)..."
+	if [[ ! -x ${LC0} ]]; then
+		echo "ERROR: Please install/update and compile Leela (lc0) in order to run tests."
+		echo
+		exit 1
+	fi
 
-LC0="${LC0:-./test/lc0/build/release/lc0}"
-echo -e "\n\n===== Attempting to test with Leela (lc0)..."
-if [[ ! -x ${LC0} ]]; then
-	echo "ERROR: Please install/update and compile Leela (lc0) in order to run tests."
-	echo
-	exit 1
+	c3c compile-run ${TEST_OPTS} ./test/leela.c3 -- "${LC0}" \
+		|| { echo "Leela (lc0) test failed. Aborting." && exit 1; }
 fi
-
-c3c compile-run ${TEST_OPTS} ./test/leela.c3 -- "${LC0}" \
-	|| { echo "Leela (lc0) test failed. Aborting." && exit 1; }
